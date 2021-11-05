8356c7cbfcd2af028ada1e1d3c9b95d7c09a3310
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2021-11-05 05:17:19 +0000

    Finish implementing + testing parser

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,7 +4,7 @@
 //! The implementation is nearly fully complete. Only image tags are missing. They are just ignored by now.
 //!
 
-use pest_consume::match_nodes;
+use pest_consume::{match_nodes, Parser};
 
 #[derive(pest_derive::Parser)]
 #[grammar = "../CookLang.pest"]
@@ -31,7 +31,7 @@ type Step<'a> = Vec<StepItem<'a>>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum StepItem<'a> {
-    Content(String),
+    Content(&'a str),
     Ingredient(Ingredient<'a>),
     Cookware(Cookware<'a>),
     Timer(Timer<'a>),
@@ -56,7 +56,17 @@ pub struct Timer<'a> {
 
 #[pest_consume::parser]
 impl CookParser {
+    fn EOI(_input: Node) -> Pesult<()> {
+        Ok(())
+    }
+
     fn recipe(input: Node) -> Pesult<Vec<RecipeLine>> {
+        Ok(match_nodes!(input.into_children();
+            [recipe_main(o), EOI(_)] => o,
+        ))
+    }
+
+    fn recipe_main(input: Node) -> Pesult<Vec<RecipeLine>> {
         Ok(match_nodes!(input.into_children();
             [line(l)..] => l.filter_map(|x| x).collect(),
         ))
@@ -92,10 +102,15 @@ impl CookParser {
 
     fn step_part(input: Node) -> Pesult<StepItem> {
         Ok(match_nodes!(input.into_children();
+            [step_content(i)] => StepItem::Content(i),
             [ingredient(i)] => StepItem::Ingredient(i),
             [cookware(i)] => StepItem::Cookware(i),
             [timer(i)] => StepItem::Timer(i),
         ))
+    }
+
+    fn step_content(input: Node) -> Pesult<&str> {
+        Ok(input.as_str())
     }
 
     fn ingredient(input: Node) -> Pesult<Ingredient> {
@@ -168,15 +183,145 @@ impl CookParser {
     }
 }
 
-fn parse(input: &str) -> Pesult<Vec<RecipeLine>> {
-    CookParser::parse(Rule::recipe, input)
+pub fn parse(input: &str) -> Pesult<Vec<RecipeLine>> {
+    let nodes = CookParser::parse(Rule::recipe, input)?;
+    CookParser::recipe(nodes.single()?)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn test(input: &str) {}
+    fn test(input: &str) -> Vec<RecipeLine> {
+        parse(input).unwrap()
+    }
 
-    fn test_metadata() {}
+    fn test_multi(input: &str, output: Vec<RecipeLine>) {
+        assert_eq!(test(input), output);
+    }
+
+    fn test_single(input: &str, output: RecipeLine) {
+        test_multi(input, vec![output]);
+    }
+
+    fn md<'a>(key: &'a str, value: &'a str) -> RecipeLine<'a> {
+        RecipeLine::Metadata(Metadata { key, value })
+    }
+
+    #[test]
+    fn test_metadata() {
+        test_single(">> servings: 2", md("servings", "2"));
+        test_multi(
+            "\t  >> a : b c \n \t >>3d:f g  ",
+            vec![md("a", "b c"), md("3d", "f g")],
+        );
+    }
+
+    #[test]
+    fn basic_step() {
+        test_single(
+            " do something ",
+            RecipeLine::Step(vec![StepItem::Content("do something")]),
+        );
+    }
+
+    #[test]
+    fn ingredient() {
+        let r = RecipeLine::Step(vec![
+            StepItem::Content("chop"),
+            StepItem::Ingredient(Ingredient {
+                name: "cucumber",
+                amount: "",
+            }),
+            StepItem::Content("finely"),
+        ]);
+        test_single(" chop @cucumber finely", r.clone());
+        test_single("chop@cucumber{}finely", r);
+    }
+
+    #[test]
+    fn long_ingredient() {
+        test_single(
+            "sprinkle @ground pepper{} to taste",
+            RecipeLine::Step(vec![
+                StepItem::Content("sprinkle"),
+                StepItem::Ingredient(Ingredient {
+                    name: "ground pepper",
+                    amount: "",
+                }),
+                StepItem::Content("to taste"),
+            ]),
+        );
+    }
+
+    #[test]
+    fn ingredient_quantity() {
+        test_single(
+            "chop @red bell pepper{1kg}",
+            RecipeLine::Step(vec![
+                StepItem::Content("chop"),
+                StepItem::Ingredient(Ingredient {
+                    name: "red bell pepper",
+                    amount: "1kg",
+                }),
+            ]),
+        );
+    }
+
+    #[test]
+    fn cookware() {
+        test_single(
+            "#knife",
+            RecipeLine::Step(vec![StepItem::Cookware(Cookware { name: "knife" })]),
+        );
+    }
+
+    #[test]
+    fn long_cookware() {
+        test_single(
+            "chop @cheese with #long knife{}",
+            RecipeLine::Step(vec![
+                StepItem::Content("chop"),
+                StepItem::Ingredient(Ingredient {
+                    name: "cheese",
+                    amount: "",
+                }),
+                StepItem::Content("with"),
+                StepItem::Cookware(Cookware { name: "long knife" }),
+            ]),
+        );
+    }
+
+    #[test]
+    fn timer() {
+        test_single(
+            "cook @eggs{2} in #skillet for ~{25%minutes}.",
+            RecipeLine::Step(vec![
+                StepItem::Content("cook"),
+                StepItem::Ingredient(Ingredient {
+                    name: "eggs",
+                    amount: "2",
+                }),
+                StepItem::Content("in"),
+                StepItem::Cookware(Cookware { name: "skillet" }),
+                StepItem::Content("for"),
+                StepItem::Timer(Timer {
+                    duration: 25,
+                    unit: "minutes",
+                }),
+                StepItem::Content("."),
+            ]),
+        );
+    }
+
+    #[test]
+    fn blank_lines() {
+        test_multi(
+            "       a       \n    \n\nb\n\n",
+            vec![
+                RecipeLine::Step(vec![StepItem::Content("a")]),
+                RecipeLine::Step(vec![StepItem::Content("b")]),
+            ],
+        );
+    }
 }
